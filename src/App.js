c5154a586e0fd88812da26064b9dcfb527ce368e
Jai Prakash <jp.pingal1993@gmail.com>
2022-05-04 13:56:22 +0530

    Home component created and header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-    <h1>Hello World</h1>
+    <Home/>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+       <div className='home-container'>
+           <header>
+               <span className='header-title'>To Do List</span>
+               <span className='add-task'>To Do List</span>
+               
+           </header>
+       </div>
+    )
+};
+export default Home;
